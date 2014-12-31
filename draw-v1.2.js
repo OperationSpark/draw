a4ffa3d5211b434b080b451fd0734b2d116e81e7
John Fraboni <johnfraboni@hotmail.com>
2014-12-31 00:35:49 +0000

    * add support for draw on same shape type

--- draw-v1.2.js
+++ draw-v1.2.js
@@ -1,12 +1,65 @@
 /**
- * draw.js : a wrapper of the CreateJS Graphic API to reduce boiler-plate.
- * version: 1.2
+ * draw.js : Not optimally desgiend, but to assist in learning, a wrapper 
+ * of the CreateJS Graphic API to reduce boiler-plate.  For CreateJS Graphic 
+ * API not wrapped by this version, use the Graphic API directly.
+ * 
+ * See: http://www.createjs.com/Docs/EaselJS/classes/Graphics.html
+ * 
+ * Version: 1.2
+ * 
+ * Dependencies: easeljs-0.7.1
  */
 (function (window) {
 
-    window.opspark = window.opspark || {}
+    window.opspark = window.opspark || {};
+    
+    function sortNumbersAscending(a, b) { return a - b; }
     
     var draw = {
+        setBoundsOn: function (shape, width, height, xOffset, yOffset, radius, onShape) {
+            if (onShape) {
+        	    // first figure out the points of extremity //
+        	    var xStartPoint = [onShape.xOffset, xOffset].sort(sortNumbersAscending)[0];
+        	    var xEndPoint = [onShape.xOffset + onShape.width, xOffset + width].sort(sortNumbersAscending)[1];
+        	    
+        	    var yStartPoint = [onShape.yOffset, yOffset].sort(sortNumbersAscending)[0];
+        	    var yEndPoint = [onShape.yOffset + onShape.height, yOffset + height].sort(sortNumbersAscending)[1];
+        	    
+        	    var xs = 0;
+                var ys = 0;
+                
+                /*
+                 * for the width calculation, we don't care about the y value 
+                 * of the points of comparison, and vice versa for height.
+                 */
+                xs = xEndPoint - xStartPoint;
+                xs = xs * xs;
+                width = Math.sqrt(xs + ys);
+        	    
+                xs = 0;
+                ys = yEndPoint - yStartPoint;
+                ys = ys * ys;
+        	    height = Math.sqrt(xs + ys);
+        	    
+        	    xOffset = xStartPoint, yOffset = yStartPoint;
+        	    
+        	    // TODO: Hmm, haven't checked this is right or even valid on 
+        	    // compound shapes, regardless, we are not taking into account offset, so, fix it or drop it //
+        	    if (radius) { radius += onShape.radius; }
+        	}
+            
+            shape.setBounds(xOffset, yOffset, width, height);
+        	shape.bounds = shape.getBounds();
+        	shape.width = shape.bounds.width;
+        	shape.height = shape.bounds.height;
+        	shape.xOffset = xOffset;
+        	shape.yOffset = yOffset;
+        	if (radius) {shape.radius = radius; }
+        	console.log(width);
+        	console.log(height);
+	        return shape;
+        },
+        
         line: function (x, y, strokeColor, strokeStyle, xOffset, yOffset, shape) {
             xOffset = (xOffset) ? xOffset : 0;
             yOffset = (yOffset) ? yOffset : 0;
@@ -18,110 +71,109 @@
                 .lineTo(x, y);
             return shape;
         },
-
-        rect: function (width, height, color, strokeColor, strokeStyle, xOffset, yOffset, shape) { 
+        
+        rect: function (width, height, color, strokeColor, strokeStyle, xOffset, yOffset, onShape) { 
         	xOffset = (xOffset) ? xOffset : 0;
         	yOffset = (yOffset) ? yOffset : 0;
         	width = (width) ? width : 0;
         	height = (height) ? height : 0;
-        	shape = (shape) ? shape : new createjs.Shape();
+        	
+        	var shape = (onShape) ? onShape : new createjs.Shape();
+        	
         	shape.graphics
         		.setStrokeStyle(strokeStyle)
         		.beginStroke(strokeColor)
         		.beginFill(color)
         		.drawRect(xOffset, yOffset, width, height);
-        	shape.setBounds(xOffset, yOffset, width, height);
-        	shape.bounds = shape.getBounds();
-        	shape.width = width;
-        	shape.height = height;
-        	shape.xOffset = xOffset;
-        	shape.yOffset = yOffset;
-	        return shape;
-    	},
-
-    	roundRect: function (width, height, radius, color, strokeColor, strokeStyle, xOffset, yOffset, shape) {
+        	
+        	return draw.setBoundsOn(shape, width, height, xOffset, yOffset, null, onShape);
+        },
+        
+        roundRect: function (width, height, radius, color, strokeColor, strokeStyle, xOffset, yOffset, onShape) {
     		xOffset = (xOffset) ? xOffset : 0;
         	yOffset = (yOffset) ? yOffset : 0;
         	width = (width) ? width : 0;
         	height = (height) ? height : 0;
-        	shape = (shape) ? shape : new createjs.Shape();
+        	var shape = (onShape) ? onShape : new createjs.Shape();
         	shape.graphics
         		.setStrokeStyle(strokeStyle)
         		.beginStroke(strokeColor)
         		.beginFill(color)
         		.drawRoundRect(xOffset, yOffset, width, height, radius);
-        	shape.setBounds(xOffset, yOffset, width, height);
-        	shape.bounds = shape.getBounds();
-        	shape.width = width;
-        	shape.height = height;
-        	shape.radius = radius;
-        	shape.xOffset = xOffset;
-        	shape.yOffset = yOffset;
-	        return shape;
+	        return draw.setBoundsOn(shape, width, height, xOffset, yOffset, radius, onShape);
+    	},
+    	
+    	roundRectComplex: function (width, 
+    	                            height, 
+    	                            radiusTopLeft, 
+    	                            radiusTopRight, 
+    	                            radiusBottomRight, 
+    	                            radiusBottomLeft, 
+    	                            color, 
+    	                            strokeColor, 
+    	                            strokeStyle, 
+    	                            xOffset, 
+    	                            yOffset, 
+    	                            onShape) {
+    	    xOffset = (xOffset) ? xOffset : 0;
+        	yOffset = (yOffset) ? yOffset : 0;
+        	width = (width) ? width : 0;
+        	height = (height) ? height : 0;
+        	var shape = (onShape) ? onShape : new createjs.Shape();
+        	shape.graphics
+        		.setStrokeStyle(strokeStyle)
+        		.beginStroke(strokeColor)
+        		.beginFill(color)
+        		.drawRoundRectComplex(xOffset, yOffset, width, height, radiusTopLeft, radiusTopRight, radiusBottomRight, radiusBottomLeft);
+	        draw.setBoundsOn(shape, width, height, xOffset, yOffset, null, onShape);
+	        shape.radiusTopLeft = radiusTopLeft;
+	        shape.radiusTopRight = radiusTopRight;
+	        shape.radiusBottomRight = radiusBottomRight;
+	        shape.radiusBottomLeft = radiusBottomLeft;
+            return shape;
     	},
 
-    	circle: function (radius, color, strokeColor, strokeStyle, xOffset, yOffset, shape) { 
+    	circle: function (radius, color, strokeColor, strokeStyle, xOffset, yOffset, onShape) { 
         	xOffset = (xOffset) ? xOffset : 0;
         	yOffset = (yOffset) ? yOffset : 0;
-        	width = (radius * 2) + xOffset;
-        	height = (radius * 2) + yOffset;
-        	shape = (shape) ? shape : new createjs.Shape();
+        	var width = (radius * 2);
+        	var height = (radius * 2);
+        	var shape = (onShape) ? onShape : new createjs.Shape();
         	shape.graphics
         		.setStrokeStyle(strokeStyle)
         		.beginStroke(strokeColor)
         		.beginFill(color)
         		.drawCircle(xOffset, yOffset, radius);
-        	shape.setBounds(xOffset, yOffset, width, height);
-        	shape.bounds = shape.getBounds();
-        	shape.width = width;
-        	shape.height = height;
-        	shape.radius = radius;
-        	shape.xOffset = xOffset;
-        	shape.yOffset = yOffset;
-	        return shape;
+        	
+        	return draw.setBoundsOn(shape, width, height, xOffset, yOffset, radius, onShape);
     	},
 
-        drawEllipse: function (width, height, color, strokeColor, strokeStyle, xOffset, yOffset, shape) {
+        drawEllipse: function (width, height, color, strokeColor, strokeStyle, xOffset, yOffset, onShape) {
             xOffset = (xOffset) ? xOffset : 0;
             yOffset = (yOffset) ? yOffset : 0;
-            width = (radius * 2) + xOffset;
-            height = (radius * 2) + yOffset;
-            shape = (shape) ? shape : new createjs.Shape();
+            var shape = (onShape) ? onShape : new createjs.Shape();
             shape.graphics
                 .setStrokeStyle(strokeStyle)
                 .beginStroke(strokeColor)
                 .beginFill(color)
                 .drawEllipse(xOffset, yOffset, width, height);
-            shape.setBounds(xOffset, yOffset, width, height);
-            shape.bounds = shape.getBounds();
-            shape.width = width;
-            shape.height = height;
-            shape.xOffset = xOffset;
-            shape.yOffset = yOffset;
-            return shape;
+            return draw.setBoundsOn(shape, width, height, xOffset, yOffset, null, onShape);
         },
 
-    	polyStar: function (radius, sides, pointSize, angle, color, strokeColor, strokeStyle, xOffset, yOffset, shape) {
+    	polyStar: function (radius, sides, pointSize, angle, color, strokeColor, strokeStyle, xOffset, yOffset, onShape) {
     		xOffset = (xOffset) ? xOffset : 0;
         	yOffset = (yOffset) ? yOffset : 0;
-        	width = (radius * 2) + xOffset;
-        	height = (radius * 2) + yOffset;
-        	shape = (shape) ? shape : new createjs.Shape();
+        	var width = (radius * 2);
+        	var height = (radius * 2);
+        	var shape = (onShape) ? onShape : new createjs.Shape();
         	shape.graphics
         		.setStrokeStyle(strokeStyle)
         		.beginStroke(strokeColor)
         		.beginFill(color)
         		.drawPolyStar(xOffset, yOffset, radius, sides, pointSize || 0, angle);
-        	shape.setBounds(xOffset, yOffset, width, height);
-        	shape.bounds = shape.getBounds();
-        	shape.width = width;
-        	shape.height = height;
-        	shape.radius = radius;
-        	shape.xOffset = xOffset;
-        	shape.yOffset = yOffset;
-	        return shape;
+        	return draw.setBoundsOn(shape, width, height, xOffset, yOffset, radius, onShape);
     	} 
-    }
+    };
     
 	window.opspark.draw = draw;
 
--- test/js/view.js
+++ test/js/view.js
@@ -1,6 +1,7 @@
 (function (window) {
 
     window.index = window.index || {};
+    var should = window.should;
     var draw = window.opspark.draw;
 
     /*
@@ -16,14 +17,69 @@
 
     var p = View.prototype = new createjs.Container();
     p.Container_initialize = p.initialize;
-
-     */
+    
     p.initialize = function () {
-        should(draw.rect);
-    }
+        var shapeOne;
+        
+        /*
+         * API : draw.rect(width, height, color, strokeColor, strokeStyle, xOffset, yOffset, shape)
+         */
+        shapeOne = draw.rect(40, 40, '#CCC', null, null, 0, 0);
+        
+        /*
+         * for the purpose of visually developing the test, place shapes
+         * where we'll be able to see any offset graphics added to them.
+         */
+        shapeOne.x = 10;
+        view.addChild(shapeOne);
+        
+        (shapeOne.width).should.be.exactly(40);
+        
+        draw.rect(10, 40, '#999', null, null, -10, 20, shapeOne);
+        (shapeOne.width).should.be.exactly(50);
+        (shapeOne.height).should.be.exactly(60);
+        (shapeOne.xOffset).should.be.exactly(-10);
+        
+        draw.rect(10, 10, '#444', null, null, 10, 40, shapeOne);
+        
+        // API : draw.circle(radius, color, strokeColor, strokeStyle, xOffset, yOffset, onShape)
+        var shapeTwo;
+        shapeTwo = draw.circle(40, '#CCC');
+        shapeTwo.x = shapeTwo.y = 80;
+        view.addChild(shapeTwo);
+        
+        (shapeTwo.width).should.be.exactly(80);
+        (shapeTwo.height).should.be.exactly(80);
+        
+        draw.circle(40, '#444', null, null, 40, 40, shapeTwo);
+        (shapeTwo.width).should.be.exactly(120);
+        (shapeTwo.height).should.be.exactly(120);
+        
+        var shapeThree = draw.circle(20, '#CCC');
+        shapeThree.x = shapeThree.y = 200;
+        view.addChild(shapeThree);
+        
+        (shapeThree.width).should.be.exactly(40);
+        (shapeThree.height).should.be.exactly(40);
+        
+        draw.circle(20, '#444', null, null, -20, -20, shapeThree);
+        (shapeThree.width).should.be.exactly(60);
+        (shapeThree.height).should.be.exactly(60);
+        
+        var shapeFour = draw.rect(40, 40, '#CCC', null, null, 0, 0);
+        shapeFour.x = 80, shapeFour.y = 220;
+        view.addChild(shapeFour);
+        
+        // breaks down here :{
+        // draw circle onto rectangular shape //
+        draw.circle(40, '0x999', null, null, 20, 20, shapeFour);
+        
+        
+        console.log('all tests are cool!');
+    };
 
     p.update = function () {
-    }
+    };
 
 
     /*
